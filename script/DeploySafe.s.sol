a54d6ecce6845ca9bcf3651a698a77e1a4bede43
SwissQuant <rico@nemo.ventures>
2024-03-29 13:03:31 +0000

    deployment script adjusted

--- script/DeploySafe.s.sol
+++ script/DeploySafe.s.sol
@@ -8,14 +8,14 @@ import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 contract DeploySafe is Script {
     Safe public safe;
 
-    function run(address ADMIN_ADDRESS, address MANAGER_ADDRESS) external payable {
+    function run(address OWNER) external payable {
         vm.startBroadcast();
 
         // Deploying the Safe contract
         safe = new Safe();
 
         // Encode the initializer function call
-        bytes memory initData = abi.encodeWithSelector(Safe.initialize.selector, ADMIN_ADDRESS, MANAGER_ADDRESS);
+        bytes memory initData = abi.encodeWithSelector(Safe.initialize.selector, OWNER);
 
         // Deploy the proxy, pointing to the implementation and including initializer call
         new ERC1967Proxy(address(safe), initData);
